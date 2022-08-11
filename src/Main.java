98e160267cc89fe33748e486c13bb58ac4dfbcdc
Dmitry Shishkin <4144442@bk.ru>
2022-08-11 09:30:41 +0300

    2 задание

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] data = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        if (data[3] <= data[1]) System.out.println(data[0]);
-        else System.out.println(data[0] + (data[3] - data[1]) * data[2]);
+        System.out.println((int) Math.ceil(Math.log(in.nextInt()) / Math.log(2)));
     }
 }
