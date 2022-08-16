2ce7a4070855f6781ce75b4a3896c84c9a4363ea
Dmitry Shishkin <4144442@bk.ru>
2022-08-16 10:05:15 +0300

    4 задание

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,42 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public int n;
-    public int[] values;
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Main main = new Main();
-        main.n = in.nextInt();
-        in.nextLine();
-        main.values = Arrays.stream(in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        System.out.println(main.sum());
+        List<String> lines = new ArrayList<>();
+        String line;
+        while ((line = in.nextLine()) != null && !line.isEmpty()) {
+            lines.add(line);
+        }
+        recursive(lines, new HashMap<>(), 0);
     }
 
-    public int sum() {
-        return Arrays.stream(values).sum() - sumDiscount(0, new ArrayList<>(), 0);
-    }
-
-    public int sumDiscount(int i, List<Integer> maxValues, int maxSum) {
-        List<Integer> newMaxValues = null;
-        while (i < n) {
-            if (values[i] >= 100) {
-                newMaxValues = new ArrayList<>(maxValues);
-                newMaxValues.add(0);
-                maxSum = sumDiscount(i + 1, newMaxValues, maxSum);
+    public static int recursive(List<String> lines, Map<String, Integer> map, int i) {
+        while (i < lines.size()) {
+            if (lines.get(i).equals("{")) {
+                i = recursive(lines, new HashMap<>(map), i + 1);
+            } else if (lines.get(i).equals("}"))
+                return i+1;
+            else {
+                String[] splitLines = lines.get(i).split("=");
+                int a = 0;
+                if (map.containsKey(splitLines[1])) {
+                    map.put(splitLines[0], map.get(splitLines[1]));
+                    System.out.println(map.get(splitLines[1]));
+                } else {
+                    try {
+                        a = Integer.parseInt(splitLines[1]);
+                    } catch (NumberFormatException e) {
+                        a = 0;
+                        System.out.println(0);
+                    } finally {
+                        map.put(splitLines[0], a);
+                    }
+                }
+                i++;
             }
-            maxValues = changeMinValue(maxValues, values[i]);
-            i++;
         }
-        return Math.max(maxSum, maxValues.stream().mapToInt(Integer::intValue).sum());
-    }
-
-    private List<Integer> changeMinValue(List<Integer> maxValues, int forChange) {
-        if (maxValues == null || maxValues.size() == 0)
-            return maxValues;
-        int minInd = 0;
-        for (int i = 1; i < maxValues.size(); i++) {
-            if (maxValues.get(i) < maxValues.get(minInd))
-                minInd = i;
-        }
-        if (forChange > maxValues.get(minInd))
-            maxValues.set(minInd, forChange);
-        return maxValues;
+        return i;
     }
 }
--- src/SimpleTests.java
+++ src/SimpleTests.java
@@ -1,52 +1,8 @@
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class SimpleTests {
 
-    private Main main;
-
-    @Before
-    public void main() {
-        main = new Main();
-    }
-
     @Test
     public void testSimple1() {
-        main.n = 5;
-        main.values = new int[]{35, 40, 101, 59, 63};
-        int actual = main.sum();
-        Assert.assertEquals(235, actual);
-    }
-
-    @Test
-    public void testSimple2() {
-        main.n = 5;
-        main.values = new int[]{35, 40, 99, 59, 63};
-        int actual = main.sum();
-        Assert.assertEquals(296, actual);
-    }
-
-    @Test
-    public void testSimple3() {
-        main.n = 6;
-        main.values = new int[]{101, 1, 2, 102, 1, 2};
-        int actual = main.sum();
-        Assert.assertEquals(107, actual);
-    }
-
-    @Test
-    public void testSimple4() {
-        main.n = 4;
-        main.values = new int[]{101, 1, 102, 1};
-        int actual = main.sum();
-        Assert.assertEquals(103, actual);
-    }
-    @Test
-    public void testSimple5() {
-        main.n = 4;
-        main.values = new int[]{101, 1, 102, 123};
-        int actual = main.sum();
-        Assert.assertEquals(203, actual);
     }
 }
